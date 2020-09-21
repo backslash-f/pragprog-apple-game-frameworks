056d71ce210afd7e53483eff0f056bb67b3b1a25
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-09-21 10:26:12 +0200

    Add background assets

--- gloop-drop/gloop-drop Shared/Scene/GameScene/GameScene.swift
+++ gloop-drop/gloop-drop Shared/Scene/GameScene/GameScene.swift
@@ -7,4 +7,48 @@
 
 import SpriteKit
 
-class ​GameScene​: GloopDropScene {}
+class ​GameScene​: GloopDropScene {
+
+    // MARK: - Lifecycle
+
+    init(size: CGSize) {
+        super.init(size: size)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+}
+
+// MARK: - SpriteKit
+
+extension ​GameScene​ {
+
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        setupBackground()
+    }
+}
+
+// MARK: - Private
+
+private extension ​GameScene​ {
+
+    func setup() {
+        view?.ignoresSiblingOrder = false
+        scaleMode = .aspectFill
+        backgroundColor = UIColor(
+            red: 105/255,
+            green: 157/255,
+            blue: 181/255,
+            alpha: 1.0
+        )
+    }
+
+    func setupBackground() {
+        let background = SKSpriteNode(imageNamed: "background_1")
+        addChild(background)
+    }
+}
