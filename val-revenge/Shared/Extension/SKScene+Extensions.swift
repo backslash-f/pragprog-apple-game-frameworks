e2d523f33f4edb8ead5fc60b21a066bb0280e84b
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-12-17 15:51:31 +0100

    Handle macOS coordinates

--- val-revenge/Shared/Extension/SKScene+Extensions.swift
+++ val-revenge/Shared/Extension/SKScene+Extensions.swift
@@ -10,14 +10,20 @@ import SpriteKit
 
 extension SKScene {
 
+    /// Converts the "top view coordinate" to a "top scene coordinator" and returns it.
     var viewTop: CGFloat {
-        convertPoint(fromView: .zero).y
+        guard let skView = view else {
+            return .zero
+        }
+        return convertPoint(fromView: topView(in: skView)).y
     }
 
+    /// Converts the "left view coordinate" to a "bottom scene coordinator" and returns it.
     var viewLeft: CGFloat {
         convertPoint(fromView: .zero).x
     }
 
+    /// Converts the "right view coordinate" to a "bottom scene coordinator" and returns it.
     var viewRight: CGFloat {
         guard let view = view else {
             return .zero
@@ -26,12 +32,12 @@ extension SKScene {
         return convertPoint(fromView: point).x
     }
 
+    /// Converts the "bottom view coordinate" to a "bottom scene coordinator" and returns it.
     var viewBottom: CGFloat {
-        guard let view = view else {
+        guard let skView = view else {
             return .zero
         }
-        let point = CGPoint(x: .zero, y: view.bounds.size.height)
-        return convertPoint(fromView: point).y
+        return convertPoint(fromView: bottomView(in: skView)).y
     }
 
     #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
@@ -39,4 +45,35 @@ extension SKScene {
         UIApplication.shared.delegate?.window??.safeAreaInsets ?? .zero
     }
     #endif
+}
+
+// MARK: - Private
+
+private extension SKScene {
+
+    // MARK: Coordinate System
+
+    /*
+     macOS uses a different coordinate systems. These functions handle that.
+     */
+
+    func topView(in skView: SKView) -> CGPoint {
+        let topY: CGPoint
+        #if os(iOS) || os(tvOS) || os(watchOS)
+        topY = .zero
+        #elseif os(OSX)
+        topY = CGPoint(x: .zero, y: skView.bounds.size.height)
+        #endif
+        return topY
+    }
+
+    func bottomView(in skView: SKView) -> CGPoint {
+        let bottomY: CGPoint
+        #if os(iOS) || os(tvOS) || os(watchOS)
+        bottomY = CGPoint(x: .zero, y: skView.bounds.size.height)
+        #elseif os(OSX)
+        bottomY = .zero
+        #endif
+        return bottomY
+    }
 }
